98cbdf4187cca2d1c01b5571195f547463b45ea1
charan <subbvad@gmail.com>
2020-09-23 20:44:11 +0530

    jenkin test

---
+++ src/main/java/com/student/dal/repos/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.student.dal.repos;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.student.dal.entities.Student;
+
+public interface StudentRepository extends CrudRepository<Student, Long> {
+
+}
---
+++ src/test/java/com/student/dal/StudentdalApplicationTests.java
@@ -0,0 +1,28 @@
+package com.student.dal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.student.dal.entities.Student;
+import com.student.dal.repos.StudentRepository;
+
+@SpringBootTest
+class StudentdalApplicationTests {
+
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@Test
+	public void testCreateStudent() {
+		
+		Student student = new Student();
+		student.setName("John");
+		student.setCourse("Java Lang");
+		student.setFee(30d);
+		
+		studentRepository.save(student);
+	}
+
+
+}
